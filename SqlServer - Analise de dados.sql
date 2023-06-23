6fcea7d138820c39b0da3fd9fe499087ea7af754
wendellbarcelos <wendell_barcelos2@hotmail.com>
2023-06-23 18:20:06 -0300

    Att1

--- SqlServer - Analise de dados.sql
+++ SqlServer - Analise de dados.sql
@@ -85,7 +85,52 @@ nessa informação */
 SELECT COUNT(DISTINCT ProductKey) AS Qtd_Vendida FROM FactSales
 -- Tem apenas um produto não vendido necessário invistigar mais afundo
 
+-- Ex A Empresa gostaria de presentear todas as clientes que moram na Europa para lhes 
+-- e sejam acima de 40 anos, porem esta no mês das mulheres com a letra M. 
+-- Usando a função Datediff para criar a coluna idade e usado no Where
+
+SELECT DISTINCT CONCAT(FirstName,' ', LastName) AS Nome_Cliente,  
+	Gender AS Sexo,
+	BirthDate AS Data_Nascimento,
+	DATEDIFF(YEAR, BirthDate,'2023/06/22' ) AS Idade,
+	ContinentName AS Continente,
+	EmailAddress AS Email,
+	DateFirstPurchase AS Primeira_Compra
+FROM DimCustomer, DimGeography
+WHERE Gender = 'F' AND (DATEDIFF(YEAR, BirthDate,'2023/06/22' ) BETWEEN 40 AND 45) AND FirstName LIKE 'M%' AND ContinentName = 'Europe';
+
+-- Ex. A o setor financeiro necessita das projeções das vendas do mês de Jan a Mar
+-- solicitando a quant vendida, total de vendas e custo, e o lucro
+
 -- Base de dados muito grande
-SELECT TOP(100) ProductName, SalesQuantity, SalesAmount 
-FROM DimProduct, FactSales
-ORDER BY NEWID();
+SELECT TOP(1000)
+	ROUND(SUM(UnitCost), 2) AS Preco_Custo_Un, 
+	ROUND(SUM(UnitPrice), 2) AS Preco_Venda_Un, 
+	SUM(SalesQuantity) AS Qtd_Vendas,
+	AVG(SalesQuantity) AS Media_Vendas,
+	SUM(TotalCost) AS Total_Custo, 
+	ROUND(SUM(SalesAmount), 2) AS Total_Vendas,
+	ROUND(SUM(SalesAmount - TotalCost), 2) AS Lucro	
+FROM FactSales;  
+-- buscar forma de trazer os valores por periodo
+
+-- O setor comercial solicitou a quantidade de clientes de cada país. O resultado deverá
+-- ter o nome do país e a quantidade de clientes em ordem decrescente de número de clientes
+
+SELECT RegionCountryName AS Paises, COUNT(FirstName) AS Qtd_Clientes 
+FROM DimCustomer, DimGeography
+WHERE RegionCountryName IS NOT NULL
+GROUP BY RegionCountryName
+ORDER BY Qtd_Clientes DESC
+
+
+-- Usando duas tabelas, idealizando consulta
+SELECT 
+	CONCAT(FirstName,' ', LastName) AS NomeCliente,
+	BirthDate AS Data_Nasc,
+	Occupation AS Profissao,
+	AddressLine1 AS Endereco,
+	CityName AS Nome_Cidade, 
+	RegionCountryName AS Nome_Pais 
+FROM DimCustomer, DimGeography
+WHERE RegionCountryName IS NOT NULL AND CityName IS NOT NULL
