c37b10c1c3bb7a68738b098c17169685723b0c24
wendellbarcelos <wendell_barcelos2@hotmail.com>
2024-10-06 20:55:27 -0300

    Iniciando novas consultas

---
+++ Aula3.sql
@@ -0,0 +1,9 @@
+/*Durante os nossos estudos, vamos conhecer o projeto da empresa Serenatto café e bistrô. Atualmente, a empresa armazena as suas informações
+em diversas fontes de dados, e nosso trabalho é centralizar todos esses dados em um só lugar e extrair informações essenciais para os gestores 
+da empresa. Então, vamos utilizar o SQLite Online, para criar as nossas tabelas e executar as nossas consultas utilizando a linguagem SQL.*/
+
+-- Tabelas: Produtos, Clientes, Pedidos, Itens de Pedido, Colaboradores, Fornecedores
+
+SELECT * FROM Clientes;
+
+
